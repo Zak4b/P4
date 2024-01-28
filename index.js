4c82e2531be91630f551c092fc3da431adb4a94d
zakab <zakab.66@gmail.com>
2024-01-28 23:48:59 +0100

    v1.2.1

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const { createReadStream } = require("node:fs");
 
 const server = http.createServer(async (req, res) => {
 	const url = new URL(req.url, `http://${req.headers.host}`);
-	//console.log("req.url", req.url, `http://${req.headers.host}`, url.pathname);
 
 	const stream = createReadStream("./ahh.html");
 	stream.pipe(res);
@@ -19,6 +18,7 @@ server.listen(process.env.PORT, process.env.IP, () => {
 
 const rooms = new GameRoomList(2, P4);
 const wss = new WebSocket.Server({ noServer: true });
+const regAct = /^[a-z]+$/i;
 
 wss.on("connection", (socket) => {
 	console.log("Nouvelle connexion WebSocket");
@@ -34,48 +34,59 @@ wss.on("connection", (socket) => {
 			return;
 		}
 		const { act: action, data } = message;
-		switch (action) {
-			case "join":
-				try {
-					rooms.join(data, player);
-				} catch (error) {
-					console.warn(`${player.uuid} : Salle #${data} pleine`);
-					return;
-				}
-				player.send("joined", { roomid: player.room.id, playerId: player.playerId });
-				player.send("sync", { board: player.room.game.board, cPlayer: player.room.game.cPlayer });
-				break;
-			case "play":
-				const x = data;
-				if (player.room.game.win) {
-					return;
-				}
-				if (player.playerId != player.room.game.cPlayer) return;
-				const y = player.room.game.play(player.playerId, x);
-				if (y < 0) {
-					console.log("Forbiden");
-				} else {
-					player.room.send("play", { playerId: player.playerId, x, y, nextPlayerId: player.room.game.cPlayer });
-					if (player.room.game.check(x, y)) {
-						player.room.send("game-win", { uuid: player.uuid, playerid: player.playerId });
-					} else if (player.room.game.full) {
-						player.room.send("game-full");
-					}
-				}
-				break;
-			case "restart":
-				player.room.game.setDefault();
-				player.room.send("restart");
-				break;
-			case "message":
-				typeof data === "string" && data && player.room.send("message", { clientId: player.uuid, message: data });
-				break;
-			case "debug":
-				console.log(rooms);
-				console.log(player);
-			default:
-				break;
+		if (regAct.test(String(action))) socket.emit(`act-${action}`, data);
+	});
+	socket.on("act-join", (data) => {
+		try {
+			rooms.join(data, player);
+		} catch (error) {
+			console.warn(`${player.uuid} : Salle #${data} pleine`);
+			return;
 		}
+		player.send("joined", { roomid: player.room.id, playerId: player.playerId });
+		player.send("sync", { board: player.room.game.board, cPlayer: player.room.game.cPlayer });
+	});
+	socket.on("act-play", (data) => {
+		const x = data;
+		if (player.room.game.win) {
+			return;
+		}
+		if (player.playerId != player.room.game.cPlayer) return;
+		const y = player.room.game.play(player.playerId, x);
+		if (y < 0) {
+			console.log("Forbiden");
+		} else {
+			player.room.send("play", { playerId: player.playerId, x, y, nextPlayerId: player.room.game.cPlayer });
+			if (player.room.game.check(x, y)) {
+				player.room.send("game-win", { uuid: player.uuid, playerid: player.playerId });
+			} else if (player.room.game.full) {
+				player.room.send("game-full");
+			}
+		}
+	});
+	socket.on("act-restart", (data) => {
+		player.room.game.setDefault();
+		player.room.send("restart");
+	});
+	socket.on("act-message", (data) => {
+		const text = data.trim();
+		if (text[0] != "/") {
+			if (text[0]) data && player.room.send("message", { clientId: player.uuid, message: text });
+		} else {
+			const command = text.slice(1);
+			switch (command) {
+				case "pute":
+					player.room.send(command);
+					break;
+
+				default:
+					break;
+			}
+		}
+	});
+	socket.on("act-debug", (data) => {
+		console.log(rooms);
+		console.log(player);
 	});
 });
 // Associez le serveur WebSocket au serveur HTTP
