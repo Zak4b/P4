6549ec02e20b3ee1904f2001fbeed200c4102362
zakab <zakab.66@gmail.com>
2024-01-29 23:51:09 +0100

    JS DOC

--- gameRoom.js
+++ gameRoom.js
@@ -1,10 +1,17 @@
 const uuid = require("uuid");
+const WebSocket = require("ws");
 class Player {
+	/** @type {string} */
 	#uuid = null;
+	/** @type {number} */
 	#playerId = null;
+	/** @type {WebSocket} */
 	#socket = null;
+	/** @type {GameRoom} */
 	#room = null;
-
+	/**
+	 * @param {WebSocket} socket
+	 */
 	constructor(socket) {
 		this.#socket = socket;
 		this.#uuid = uuid.v4();
@@ -14,18 +21,33 @@ class Player {
 			delete this;
 		});
 	}
+	/**
+	 * @returns {string}
+	 */
 	get uuid() {
 		return this.#uuid;
 	}
+	/**
+	 * @returns {number}
+	 */
 	get playerId() {
 		return this.#playerId;
 	}
+	/**
+	 * @param {number} id
+	 */
 	set playerId(id) {
 		this.#playerId = id;
 	}
+	/**
+	 * @returns {GameRoom}
+	 */
 	get room() {
 		return this.#room;
 	}
+	/**
+	 * @param {GameRoom} room
+	 */
 	set room(room) {
 		if (!(room instanceof GameRoom)) {
 			throw new Error("Not a GameRoom");
@@ -33,52 +55,84 @@ class Player {
 			this.#room = room;
 		}
 	}
+	/**
+	 * @param {string} act
+	 * @param {string} data
+	 */
 	send(act, data) {
 		this.#socket.send(JSON.stringify({ act, data }));
 	}
 }
 class GameRoom {
+	/** @template GameClass */
+	/** @type {string} */
 	#id = null;
+	/** @type {number} */
 	#playerMaxCount = 2;
 	#game = null;
+	/** @type {Player[]} */
 	#playerList = [];
-
+	/**
+	 * @param {string} id
+	 * @param {number} playerMaxCount
+	 * @param {new() => GameClass} gameClass
+	 */
 	constructor(id, playerMaxCount, gameClass) {
 		this.#id = id;
 		if (playerMaxCount) this.#playerMaxCount = playerMaxCount;
 		if (gameClass) this.#game = new gameClass();
 	}
+	/**
+	 * @returns {string}
+	 */
 	get id() {
 		return this.#id;
 	}
+	/**
+	 * @returns {number}
+	 */
 	get playerMaxCount() {
 		return this.#playerMaxCount;
 	}
 	get game() {
 		return this.#game;
 	}
+	/**
+	 * @returns {number}
+	 */
 	get playerCount() {
 		return this.#playerList.length;
 	}
+	/**
+	 * @returns {Player[]}
+	 */
 	get playerList() {
 		return this.#playerList;
 	}
-
+	/**
+	 * @param {Player} player
+	 */
 	join(player) {
 		if (this.#playerList.length >= this.#playerMaxCount) {
 			throw new Error("Room pleine");
 		} else if (!(player instanceof Player)) {
 			throw new Error("Not a Player");
-		} else {
-			this.#playerList.push(player);
-			player.room = this;
 		}
-		player.playerId = this.#playerList.length; // Ne marche que pour 2, modif à faire
+		player.room?.remove(player);
+		const len = this.#playerList.push(player);
+		player.playerId = len == 1 ? 1 : this.#playerList[len - 2].playerId == 1 ? 2 : 1; // Ne marche que pour 2, modif à faire
+		player.room = this;
 	}
-
+	/**
+	 * @param {Player} player
+	 */
 	remove(player) {
 		this.#playerList = this.#playerList.filter((e) => e.uuid != player.uuid);
 	}
+	/**
+	 * @param {String} act
+	 * @param {String} data
+	 */
 	send(act, data) {
 		this.#playerList.forEach((player) => {
 			player.send(act, data);
@@ -86,20 +140,35 @@ class GameRoom {
 	}
 }
 class GameRoomList {
+	/** @template GameClass */
+	/** @type {object} */
 	#list;
+	/** @type {number} */
 	#playerMaxCount;
+	/** @type {GameClass} */
 	#gameClass;
+	/**
+	 * @param {number} playerMaxCount
+	 * @param {new() => GameClass} gameClass
+	 */
 	constructor(playerMaxCount, gameClass) {
 		this.#list = {};
 		if (playerMaxCount) this.#playerMaxCount = playerMaxCount;
 		if (gameClass) this.#gameClass = gameClass;
 	}
-
+	/**
+	 * @param {string} id
+	 * @returns {GameRoom}
+	 */
 	getOrCreate(id) {
 		if (!this.#list[id]) this.#list[id] = new GameRoom(id, this.#playerMaxCount, this.#gameClass);
 		return this.#list[id];
 	}
-
+	/**
+	 * @param {string} id
+	 * @param {Player} player
+	 * @returns {GameRoom}
+	 */
 	join(id, player) {
 		return this.getOrCreate(id).join(player);
 	}
