6b0a6132d7c2cf86f629243955c8110322906ab1
zakab <zakab.66@gmail.com>
2024-01-30 23:03:49 +0100

    watcher

--- watcher.js
+++ watcher.js
@@ -1,17 +1,13 @@
-const { spawn } = require("child_process");
-const { watch } = require("fs/promises");
+import { exec, spawn } from "node:child_process";
+import { watch } from "node:fs/promises";
 const [node, _, file] = process.argv;
 
 function spawnNode() {
 	const childProcess = spawn(node, [file]);
-	childProcess.stdout.on("data", (data) => {
-		console.log(data.toString("utf8"));
-	});
-	childProcess.stderr.on("data", (data) => {
-		console.error(data.toString("utf8"));
-	});
+	childProcess.stdout.pipe(process.stdout);
+	childProcess.stderr.pipe(process.stderr);
 	childProcess.on("close", (code) => {
-		if (code > 0) throw new Error("Process exited : " + code);
+		if (code != null) process.exit(code);
 	});
 	return childProcess;
 }
@@ -21,10 +17,9 @@ async function watchFiles() {
 	const watcher = watch("./", { recursive: true });
 	for await (const event of watcher) {
 		if (event.filename.endsWith(".js")) {
-			childNodeProcess.kill();
+			childNodeProcess.kill("SIGKILL");
 			childNodeProcess = spawnNode();
 		}
 	}
 }
-
 watchFiles().catch((err) => console.error(err));
