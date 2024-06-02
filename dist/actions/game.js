da0ad658870ec5989b36d2939238b05315ad5611
zakab <zakab.66@gmail.com>
2024-06-03 01:18:48 +0200

    game Timestamp

--- dist/actions/game.js
+++ dist/actions/game.js
@@ -4,7 +4,7 @@ export const saveGame = (uuid1, uuid2, result, board) => {
     const id1 = getUser(uuid1);
     const id2 = getUser(uuid2);
     if (id1 & id2) {
-        db.prepare("INSERT INTO GAMES (player_1, player_2, result, board) VALUES (?, ?, ?, ?)").run(id1, id2, result, board);
+        db.prepare("INSERT INTO GAMES (player_1, player_2, result, board, time) VALUES (?, ?, ?, ?)").run(id1, id2, result, board, new Date().getTime());
     }
     else {
         throw new Error();
--- src/actions/game.ts
+++ src/actions/game.ts
@@ -5,7 +5,7 @@ export const saveGame = (uuid1: string, uuid2: string, result: 0 | 1 | 2, board:
 	const id1 = getUser(uuid1);
 	const id2 = getUser(uuid2);
 	if (id1 & id2) {
-		db.prepare("INSERT INTO GAMES (player_1, player_2, result, board) VALUES (?, ?, ?, ?)").run(id1, id2, result, board);
+		db.prepare("INSERT INTO GAMES (player_1, player_2, result, board, time) VALUES (?, ?, ?, ?)").run(id1, id2, result, board, new Date().getTime());
 	}else{
         throw new Error()
     }
